2740c5a34ff94c05a4421a7c38cb091a61336720
Thomas Brockington <xtombrock1989@gmail.com>
2023-12-27 18:08:14 +0000

    save

--- src/database/database.py
+++ src/database/database.py
@@ -1,6 +1,12 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_db_connection():
-    database_url = "postgres://zllryxch:nw6V9ZVwoBpdd7Zl0QqbmvH9FOX2Mggt@flora.db.elephantsql.com/zllryxch"
+    database_url = os.getenv('DATABASE_URL')
+    print(database_url)
     conn = psycopg2.connect(database_url)
     return conn
