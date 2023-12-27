088e374624b80c2868d78b6c25c7bcdcb1b6d42e
Thomas Brockington <xtombrock1989@gmail.com>
2023-12-27 21:04:14 +0000

    schema

--- database/seed.py
+++ database/seed.py
@@ -1,71 +1,15 @@
-import os
 from dotenv import load_dotenv
 import psycopg2
 import uuid
-
+from schema import create_connection, create_tables
 # Load environment variables from .env file
 load_dotenv()
 
 # Get the database URL from the environment variable
-database_url = os.getenv('DATABASE_URL')
-
-def create_connection():
-    # Use the database URL from the environment variable to establish a connection
-    return psycopg2.connect(database_url)
-
-def create_tables():
-    commands = ( 
-        """
-        CREATE TABLE IF NOT EXISTS "User" (
-            id UUID PRIMARY KEY,
-            email VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            role VARCHAR(50) NOT NULL DEFAULT 'USER',
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-        )
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS "Country" (
-            id UUID PRIMARY KEY,
-            name VARCHAR(255) NOT NULL,
-            locationId INT NOT NULL,
-            visited BOOLEAN DEFAULT TRUE,
-            user_id UUID REFERENCES "User"(id),
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-        )
-        """,
-        """
-        CREATE TABLE IF NOT EXISTS "Event" (
-            id UUID PRIMARY KEY,
-            type VARCHAR(50) NOT NULL,
-            code INT,
-            topic VARCHAR(255),
-            content TEXT,
-            created_by UUID REFERENCES "User"(id),
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-        )
-        """
-    )
-    conn = None
-    try:
-        conn = create_connection()
-        cur = conn.cursor()
-        for command in commands:
-            cur.execute(command)
-        cur.close()
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
 
 def seed_users():
     user_data = [
-        (str(uuid.uuid4()), 'user1@example.com', 'password', 'USER'),
+        (str(uuid.uuid4()), 'tom@example.com', 'password', 'USER'),
         (str(uuid.uuid4()), 'user2@example.com', 'password', 'ADMIN'),
         # Add more users as needed
     ]
